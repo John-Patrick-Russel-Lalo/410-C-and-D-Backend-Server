489da1c191a8d198909a518e62697f7b827ebe38
John-Patrick-Russel-Lalo <johnpatricklalo@gmail.com>
2025-12-04 16:19:08 +0800

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ const server = http.createServer(app);
 // CORS Config
 const allowedOrigins = [
   "http://localhost:5500",
+  "http://localhost:5173",
   "http://127.0.0.1:5500",
   "http://localhost:8080",
   "http://localhost:5501",
